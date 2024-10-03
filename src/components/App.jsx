7b0082a1008c6117ac098754a440928df7c65ea6
Alex4u18 <adrian_giurgi@yahoo.com>
2024-10-03 23:02:18 +0100

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
+const App = () => (
+  <div className="App">
+    <h1>Phonebook</h1>
+    <ContactForm />
+    <h2>Contacts</h2>
+    <Filter />
+    <ContactList />
+  </div>
+);
+
+export default App;
